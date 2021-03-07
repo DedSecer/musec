0f46f0b6718cac727889b70186b8ffe1741ab877
DedSecer <z2465216809qq.com@icloud.com>
2021-03-07 16:19:24 +0800

    fix args

--- demo.py
+++ demo.py
@@ -16,6 +16,6 @@ platform = setting['platform']
 
 
 
-#dl_song(mid, path, platform, info, uin, cookies, sformat)
-#dl_album(mid, ct=0, path, platform, info, uin, cookies, sformat)
-#dl_plist(mid, ct=0, path, platform, info, uin, cookies, sformat)
+#dl_song(mid, path=path, platform=platform, download_info=info, uin=uin, cookies=cookies, sformat=sformat, ct=0)
+#dl_album(mid, path=path, platform=platform, download_info=info, uin=uin, cookies=cookies, sformat=sformat, ct=0)
+#dl_plist(mid, path=path, platform=platform, download_info=info, uin=uin, cookies=cookies, sformat=sformat, ct=0)
